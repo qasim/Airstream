45f5d0b756ee3b3c12b951d6a448128091f47c8b
Qasim Iqbal <a-qasimi@microsoft.com>
2016-10-29 19:13:17 -0400

    Initial commit

---
+++ Airstream/Airstream.h
@@ -0,0 +1,17 @@
+//
+//  Airstream.h
+//  Airstream
+//
+//  Created by Qasim Iqbal on 10/29/16.
+//  Copyright © 2016 Qasim Iqbal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Airstream.
+FOUNDATION_EXPORT double AirstreamVersionNumber;
+
+//! Project version string for Airstream.
+FOUNDATION_EXPORT const unsigned char AirstreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Airstream/PublicHeader.h>
---
+++ Airstream/Airstream.m
@@ -0,0 +1,9 @@
+//
+//  Airstream.m
+//  Airstream
+//
+//  Created by Qasim Iqbal on 10/29/16.
+//  Copyright © 2016 Qasim Iqbal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
