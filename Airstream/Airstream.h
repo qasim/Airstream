f5ab6583e27328812d0f99a1949a6659560e76b3
Qasim Iqbal <a-qasimi@microsoft.com>
2016-10-29 20:05:13 -0400

    Add dependencies

--- Airstream/Airstream.h
+++ Airstream/Airstream.h
@@ -14,4 +14,6 @@ FOUNDATION_EXPORT double AirstreamVersionNumber;
 //! Project version string for Airstream.
 FOUNDATION_EXPORT const unsigned char AirstreamVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Airstream/PublicHeader.h>
+@interface Airstream : NSObject
+
+@end
